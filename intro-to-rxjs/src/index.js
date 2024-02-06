46e4c858a6ee07933def7405ec83bb83c4a7495e
KemarM <steff.miller@gmail.com>
2024-02-05 22:34:33 -0400

    Initial Commit

--- intro-to-rxjs/src/index.js
+++ intro-to-rxjs/src/index.js
@@ -1,9 +1,38 @@
 import { of } from "rxjs";
+import { reduce, take } from 'rxjs/operators'
 
-const observable = of(1,2,3,4,5)
+//EXAMPLE of using an observable and fetching a REST API endpoint: const observable = from(fetch('https://jsonplaceholder.typicode.com/todos/1'))
 
-const subscription = observable.subscribe( //Registering a new observer
-    console.log    
+/*EXAMPLE of pipable operators, the map() pipe operator
+const observable = of(1,2,3,4,5).pipe(
+    map((value) => `$${value}`)
 )
+*/
+
+/*const observable = fromEvent(document, 'keydown').pipe(
+    map((event) => {
+        return event.code === "Space" ? event.code : null;
+    })
+    map((event) => event.code),
+    filter((code) => code === "Space")
+);*/
+
+const observable = interval(500).pipe(
+    
+    take(),
+    reduce(
+        (acc, val) => acc + val,
+        0
+    )
+)
+//Registering a new observer
+const subscription = observable.subscribe({
+    next(value) {
+        console.log(value)
+    },
+    complete() {
+        console.log('complete')
+    }
+})
 
 console.log('hello')
