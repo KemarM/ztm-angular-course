c2fc7e55784cfc8dd4567ef9873461007df1ec36
KemarM <steff.miller@gmail.com>
2024-09-01 12:23:41 -0400

    262 - Create the FFMPEG service, it downloads a large file, hence the async and load() to check if the file already exisists in the app

--- clips/src/app/services/ffmpeg.service.ts
+++ clips/src/app/services/ffmpeg.service.ts
@@ -1,9 +1,24 @@
 import { Injectable } from '@angular/core';
+import { createFFmpeg } from '@ffmpeg/ffmpeg';
 
 @Injectable({
   providedIn: 'root'
 })
 export class FfmpegService {
+  public isReady = false
+  private ffmpeg
 
-  constructor() { }
+  constructor() {
+    this.ffmpeg = createFFmpeg({ log: true })
+  }
+
+  async init(){
+    if(this.isReady){
+      return
+    }
+
+    await this.ffmpeg.load()
+
+    this.isReady = true
+  }
 }
